9c1629a1ce1899f3a9ab2f25dd3cdac1ea18dd7f
Robert <rjf.buzink@gmail.com>
2023-09-17 21:55:19 +0200

    Fixed port mapping

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ docker stop heosdisplay
 docker rm heosdisplay
 docker rmi $(docker images 'heosdisplay:latest' -a -q)
 docker build -t heosdisplay .
-docker run --rm --name heosdisplay -P -d heosdisplay
+docker run --rm --name heosdisplay -p 3000:3000 -p 8080:8080 -d heosdisplay
 echo "http://$(ipconfig getifaddr en0):3000"
 echo "http://$(ipconfig getifaddr en1):3000"
 echo "http://localhost:3000"
